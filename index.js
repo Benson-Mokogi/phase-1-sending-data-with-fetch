935628afc182ae67f56573de149c33e0a9b9279a
Benson Mokogi <b.mokogi@gmail.com>
2023-03-30 02:57:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+document.addEventListener("DOMContentLoaded", submitData)
+
+const userData = {
+    name: "Steve",
+    email: "steve@steve.com",
+}
+
+function submitData(){
+    fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userData)
+
+    })
+
+    .then(res => res.json())
+    .then(users => createElements(users))
+    .catch(function (error){
+
+        const errorElement = document.createElement("p")
+        
+        errorElement.innerText = "Unauthorized Access"
+        document.querySelector("p").appendChild(errorElement)
+        return(error.message);
+    })
+    
+}
+
+
+function createElements(users){
+    const newElement = document.createElement("p")
+    newElement.id = users.id
+    newElement.innerText = users.name
+
+    document.querySelector("body").appendChild(newElement)
+}
